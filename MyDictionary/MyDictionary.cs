8299a83c83e5ecb5a85cca45c48d121b6ab22969
EdaKevserDemir <31306626+EdaKevserDemir@users.noreply.github.com>
2021-01-25 01:32:24 +0300

    20.01.21 Kamp Odev

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _keys;
+        TValue[] _values;
+
+        TKey[] tempKeys;
+        TValue[] tempValues;
+
+
+        public MyDictionary()
+        {
+            _keys = new TKey[0];
+            _values = new TValue[0];
+
+        }
+
+        public void Add(TKey item1, TValue item2)
+        {
+            tempKeys = _keys;
+            tempValues = _values;
+
+            _keys = new TKey[_keys.Length + 1];
+            _values = new TValue[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+            }
+
+            _keys[_keys.Length - 1] = item1;
+
+
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                _values[j] = tempValues[j];
+            }
+
+            _values[_values.Length - 1] = item2;
+
+
+        }
+
+        public TKey[] Key
+        {
+            get { return _keys; }
+        }
+
+        public TValue[] Value
+        {
+            get { return _values; }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+
+            myDictionary.Add("iyi", 16);
+            myDictionary.Add("kötü", 34);
+            myDictionary.Add("çirkin", 48);
+            myDictionary.Add("kızgın", 72);
+
+
+
+            Console.WriteLine("not :" + myDictionary.Key[0] + "  sayfa no : " + myDictionary.Value[0]);
+            Console.WriteLine("not :" + myDictionary.Key[1] + "  sayfa no : " + myDictionary.Value[1]);
+            Console.WriteLine("not :" + myDictionary.Key[2] + "  sayfa no : " + myDictionary.Value[2]);
+            Console.WriteLine("not :" + myDictionary.Key[3] + "  sayfa no : " + myDictionary.Value[3]);
+            Console.ReadLine();
+        }
+    }
+}
